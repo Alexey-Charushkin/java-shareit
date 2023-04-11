921ceda0040c2edf55e4f404a4a665a2f522a7fd
Alexey <charushkin.lexa@yandex.ru>
2023-04-11 12:40:44 +0300

    feat: создал каркас мапперов

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDto {
+
+    private Long userId;
+
+    private String name;
+
+    private String email;
+
+}
