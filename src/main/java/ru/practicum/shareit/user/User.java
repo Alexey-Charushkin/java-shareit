10eaa41256316baf7a2fe49d3b11ec48423ced88
Alexey <charushkin.lexa@yandex.ru>
2023-04-11 11:02:18 +0300

    feat: создал классы User и Item, скачал реализацию алгоритма Бойера-Мура для поиска.

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,31 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.util.List;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+
+    @NotNull
+    @Positive
+    private Long userId;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private List<Item> items;
+
 }
