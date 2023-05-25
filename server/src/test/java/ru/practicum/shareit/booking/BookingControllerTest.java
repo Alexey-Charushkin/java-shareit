0fa4d3f9da54295e3e6509b748863bce344a928e
Alexey <charushkin.lexa@yandex.ru>
2023-05-25 15:54:58 +0300

    refactor: на проверку

---
+++ server/src/test/java/ru/practicum/shareit/booking/BookingControllerTest.java
@@ -0,0 +1,105 @@
+package ru.practicum.shareit.booking;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingMapper;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BookingControllerTest {
+
+    @Mock
+    private BookingService bookingService;
+
+    @InjectMocks
+    private BookingController bookingController;
+
+    User owner = new User(0L, "userName", "email@mail.com");
+    User wrongOwner = new User(99L, "user99Name", "email99@mail.com");
+    User requestor = new User(1L, "requestorName", "requestorEmail@mail.com");
+    User booker = new User(2L, "bookerName", "bookerEmil@mail.com");
+    ItemRequest request = new ItemRequest(0L, "requestDescription", requestor, null, null);
+    Item item = new Item(0L, "itemName", "itemDescription",
+            true, owner, request);
+    Booking bookingToSave = new Booking(0L, LocalDateTime.now().plusMinutes(1), LocalDateTime.now().plusMinutes(5),
+            item, booker, "WAITING");
+
+    @Test
+    void create() {
+        BookingDto exceptedBookingDto = BookingMapper.toBookingDto(bookingToSave);
+
+        when(bookingService.create(owner.getId(), exceptedBookingDto)).thenReturn(exceptedBookingDto);
+
+        BookingDto response = bookingController.create(owner.getId(), exceptedBookingDto);
+
+        assertEquals(exceptedBookingDto.getId(), response.getId());
+        assertEquals(exceptedBookingDto.getItem(), response.getItem());
+        assertEquals(exceptedBookingDto.getBooker(), response.getBooker());
+        assertEquals(exceptedBookingDto.getStatus(), response.getStatus());
+    }
+
+    @Test
+    void approveBooking() {
+        BookingDto exceptedBookingDto = BookingMapper.toBookingDto(bookingToSave);
+        when(bookingService.approveBooking(1L, 1L, true))
+                .thenReturn(exceptedBookingDto);
+
+        BookingDto response = bookingController.approveBooking(1L, 1L, true);
+
+        assertEquals(exceptedBookingDto.getId(), response.getId());
+        assertEquals(exceptedBookingDto.getItem(), response.getItem());
+        assertEquals(exceptedBookingDto.getBooker(), response.getBooker());
+        assertEquals(exceptedBookingDto.getStatus(), response.getStatus());
+    }
+
+    @Test
+    void findByBookingId() {
+        BookingDto exceptedBookingDto = BookingMapper.toBookingDto(bookingToSave);
+        when(bookingService.findByBookingId(1L, 1L)).thenReturn(exceptedBookingDto);
+
+        BookingDto response = bookingController.findByBookingId(1L, 1L);
+
+        assertEquals(exceptedBookingDto.getId(), response.getId());
+        assertEquals(exceptedBookingDto.getItem(), response.getItem());
+        assertEquals(exceptedBookingDto.getBooker(), response.getBooker());
+        assertEquals(exceptedBookingDto.getStatus(), response.getStatus());
+    }
+
+    @Test
+    void gelAllBookingsByUserId() {
+        List<BookingDto> exceptedBookingDtoList = List.of(new BookingDto(), new BookingDto());
+        when(bookingService.getAllBookingsByUserId(1L, BookingDto.StatusDto.ALL, 0, 1))
+                .thenReturn(exceptedBookingDtoList);
+
+        List<BookingDto> actualBookingDtoList = bookingController
+                .gelAllBookingsByUserId(1L, "ALL", 0, 1);
+
+        assertEquals(exceptedBookingDtoList.size(), actualBookingDtoList.size());
+    }
+
+    @Test
+    void gelAllBookingsByOwnerId() {
+        List<BookingDto> exceptedBookingDtoList = List.of(new BookingDto(), new BookingDto());
+        when(bookingService.getAllBookingsByOwnerId(1L, BookingDto.StatusDto.ALL, 0, 1))
+                .thenReturn(exceptedBookingDtoList);
+
+        List<BookingDto> actualBookingDtoList = bookingController
+                .gelAllBookingsByOwnerId(1L, "ALL", 0, 1);
+
+        assertEquals(exceptedBookingDtoList.size(), actualBookingDtoList.size());
+    }
+}
---
+++ server/src/test/java/ru/practicum/shareit/item/ItemControllerTestIT.java
@@ -0,0 +1,229 @@
+package ru.practicum.shareit.item;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.comment.dto.CommentDto;
+import ru.practicum.shareit.comment.dto.CommentMapper;
+import ru.practicum.shareit.comment.model.Comment;
+import ru.practicum.shareit.comment.service.CommentService;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ItemMapper;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ItemController.class)
+class ItemControllerTestIT {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ItemService itemService;
+    @MockBean
+    private CommentService commentService;
+
+    User owner = new User(1L, "userName", "email@mail.com");
+    User requestor = new User(2L, "requestorName", "requestorEmail@mail.com");
+    ItemRequest request = new ItemRequest(1L, "requestDescription", requestor, null, null);
+    Item item = new Item(1L, "itemName", "itemDescription",
+            true, owner, request);
+    Item item2 = new Item(2L, "itemName2", "itemDescription2",
+            true, owner, request);
+
+    @SneakyThrows
+    @Test
+    void create() {
+        Item item = new Item(1L, "itemName", "itemDescription",
+                true, owner, request);
+        ItemDto itemDto = ItemMapper.toItemDto(item);
+        when(itemService.create(anyLong(), any(ItemDto.class))).thenReturn(itemDto);
+
+        String result = mockMvc.perform(post("/items")
+                        .header("X-Sharer-User-Id", owner.getId())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(itemDto)))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        verify(itemService, times(1)).create(anyLong(), any(ItemDto.class));
+        assertEquals(objectMapper.writeValueAsString(itemDto), result);
+
+    }
+
+    @SneakyThrows
+    @Test
+    void create_whenXSharerUserIdISEmpty() {
+        Item item = new Item(1L, "itemName", "itemDescription",
+                true, owner, request);
+        ItemDto itemDto = ItemMapper.toItemDto(item);
+        when(itemService.create(anyLong(), any(ItemDto.class))).thenReturn(itemDto);
+
+        mockMvc.perform(post("/items")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(itemDto)))
+                .andExpect(status().is5xxServerError());
+    }
+
+    @SneakyThrows
+    @Test
+    void update() {
+        Long itemId = 1L;
+        Item item = new Item(1L, "updateItemName", "updateItemDescription",
+                true, owner, request);
+        ItemDto itemDto = ItemMapper.toItemDto(item);
+        when(itemService.update(anyLong(), any(ItemDto.class))).thenReturn(itemDto);
+
+        String result = mockMvc.perform(patch("/items/{itemId}", itemId)
+                        .header("X-Sharer-User-Id", owner.getId())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(itemDto)))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        verify(itemService, times(1)).update(anyLong(), any(ItemDto.class));
+        assertEquals(objectMapper.writeValueAsString(itemDto), result);
+    }
+
+    @SneakyThrows
+    @Test
+    void getById() {
+        Long itemId = 1L;
+        mockMvc.perform(get("/items/{itemId}", itemId)
+                        .header("X-Sharer-User-Id", owner.getId()))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(itemService).getItemById(owner.getId(), itemId);
+    }
+
+    @SneakyThrows
+    @Test
+    void gelAllByUserId() {
+        Long userId = 1L;
+        Integer from = 0;
+        Integer size = 10;
+        mockMvc.perform(get("/items")
+                        .header("X-Sharer-User-Id", userId)
+                        .param("from", from.toString())
+                        .param("size", size.toString()))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(itemService).getAllItemsByUserId(userId, from, size);
+    }
+
+    @SneakyThrows
+    @Test
+    void gelAllByUserId_whenFromIsNotCorrect_thenBadRequestExceptionThrown() {
+        Long userId = 1L;
+        Integer from = -1;
+        Integer size = 10;
+        mockMvc.perform(get("/items")
+                        .header("X-Sharer-User-Id", userId)
+                        .param("from", from.toString())
+                        .param("size", size.toString()))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @SneakyThrows
+    @Test
+    void deleteById() {
+        Long itemId = 1L;
+        mockMvc.perform(delete("/items/{itemId}", itemId))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(itemService).deleteById(itemId);
+    }
+
+    @SneakyThrows
+    @Test
+    void searchItems() {
+        String query = "query Request";
+        Integer from = 0;
+        Integer size = 10;
+        mockMvc.perform(get("/items/search")
+                        .param("text", query)
+                        .param("from", from.toString())
+                        .param("size", size.toString()))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(itemService).searchItems(query, from, size);
+    }
+
+    @SneakyThrows
+    @Test
+    void searchItems_whenFromIsNotCorrect_thenBadRequestExceptionThrown() {
+        String query = "query Request";
+        Integer from = -1;
+        Integer size = 10;
+        mockMvc.perform(get("/items/search")
+                        .param("text", query)
+                        .param("from", from.toString())
+                        .param("size", size.toString()))
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @SneakyThrows
+    @Test
+    void searchItems_whenQueryIsNull() {
+        String query = "";
+        Integer from = 0;
+        Integer size = 10;
+        mockMvc.perform(get("/items/search")
+                        .param("text", query)
+                        .param("from", from.toString())
+                        .param("size", size.toString()))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @SneakyThrows
+    @Test
+    void testCreateComment() {
+        Item item = new Item(1L, "itemName", "itemDescription",
+                true, owner, request);
+        ItemDto itemDto = ItemMapper.toItemDto(item);
+        Comment comment = new Comment(1L, "Comment text", item, owner, LocalDateTime.now());
+        CommentDto commentDto = CommentMapper.toCommentDto(comment);
+
+        when(commentService.create(anyLong(), anyLong(), any(CommentDto.class))).thenReturn(commentDto);
+
+
+        String result = mockMvc.perform(post("/items/{itemId}/comment", item.getId())
+                        .header("X-Sharer-User-Id", owner.getId())
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(itemDto)))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+
+        verify(commentService, times(1)).create(anyLong(), anyLong(), any(CommentDto.class));
+        assertEquals(objectMapper.writeValueAsString(commentDto), result);
+    }
+}
