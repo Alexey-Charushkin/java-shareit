afdd7eb6e85bef9b7532412ca6a81e617ad5a4e1
Alexey <charushkin.lexa@yandex.ru>
2023-05-25 17:14:11 +0300

    feat: initial commit

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class ShareItServer {
 
