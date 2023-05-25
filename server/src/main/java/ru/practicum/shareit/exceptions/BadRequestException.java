d631d22c8301c4c25eb99a5553c7be0d64449b34
Alexey-Charushkin <113549089+Alexey-Charushkin@users.noreply.github.com>
2023-05-25 15:41:28 +0300

    Delete server directory

--- server/src/main/java/ru/practicum/shareit/exceptions/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.shareit.exceptions;
-
-public class BadRequestException extends RuntimeException {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
-
