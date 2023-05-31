8f37fbcebce40a312ba841c77dd0ce0dec7726d2
Alexey <charushkin.lexa@yandex.ru>
2023-05-31 08:57:57 +0300

    feat: на проверку

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShareItServer {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ShareItServer.class, args);
 	}
